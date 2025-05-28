8abe92057a340d0cbba2dbae22bae2fa7c536ebc
Amir <indigophi@protonmail.com>
2025-05-28 16:48:19 -0700

    Calculate testcount for each testcase

--- packages/vm/test/tester/runners/GeneralStateTestsRunner.ts
+++ packages/vm/test/tester/runners/GeneralStateTestsRunner.ts
@@ -232,6 +232,7 @@ export async function runStateTest(options: any, testData: any, t: tape.Test | C
         t.comment(`Average test run: ${(totalTimeSpent / options.reps).toLocaleString()} s`)
     } else {
       await runTestCase(options, testCase, t)
+      options.testCount++
     }
   }
 }
